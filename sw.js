4c43dacab56e2ea37ba9ac761c605cb617c9655a
aventurasencadapaso-lab <aventurasencadapaso@gmail.com>
2026-09-01 11:16:44 +0100

    summary

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Offline page para cuando no hay red
 ============================================================ */
 
-var CACHE_NAME   = 'aventuras-v12';
+var CACHE_NAME   = 'aventuras-v13';
 var BASE         = '/aventurasencadapaso';
 
 var CACHE_CORE = [
